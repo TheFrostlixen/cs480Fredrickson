ed94e8cb378c5c0b0c90b1a794b8a40ea6e511c3
Matt <mgfredrickson@nevada.unr.edu>
2015-09-10 10:06:43 -0700

    refactoring PA3

--- PA3/src/global.h
+++ PA3/src/global.h
@@ -7,14 +7,3 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include <chrono>
-
-
-//returns the time delta
-float getDT()
-{
-	float ret;
-	t2 = std::chrono::high_resolution_clock::now();
-	ret = std::chrono::duration_cast< std::chrono::duration<float> >(t2-t1).count();
-	t1 = std::chrono::high_resolution_clock::now();
-	return ret;
-}
--- PA3/src/main.cpp
+++ PA3/src/main.cpp
@@ -370,3 +370,12 @@ void cleanUp()
 	glDeleteBuffers(1, &vbo_geometry);
 }
 
+//returns the time delta
+float getDT()
+{
+	float ret;
+	t2 = std::chrono::high_resolution_clock::now();
+	ret = std::chrono::duration_cast< std::chrono::duration<float> >(t2-t1).count();
+	t1 = std::chrono::high_resolution_clock::now();
+	return ret;
+}
