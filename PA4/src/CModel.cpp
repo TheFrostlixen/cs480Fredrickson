c49e7ff2a063d84bda5d05755e57a79467cdc2a6
mgfredrickson@nevada.unr.edu <mgfredrickson@nevada.unr.edu>
2015-09-21 21:01:02 -0700

    started writing the model loader

---
+++ PA4/src/CModel.cpp
@@ -0,0 +1,119 @@
+#include "CModel.h"
+
+Model::Model()
+{
+	lat_radius = 0.0f;
+	long_radius = 0.0f;
+	isSpinning = true;
+	spinDirection = false;
+	angleRotation = 0.0f;
+	angleTranslation = 0.0f;
+}
+
+Model::~Model()
+{
+	lat_radius = 0.0f;
+	long_radius = 0.0f;
+	isSpinning = false;
+	spinDirection = false;
+	angleRotation = 0.0f;
+	angleTranslation = 0.0f;
+}
+
+int Model::LoadModel( std::string path )
+{
+    // This is what the data will be read in to
+    std::string src = "";
+    int vertices = 0;
+
+    // Set up file IO ops
+    std::ifstream fin(path.c_str());
+    std::stringstream stream;
+
+    // Clear any file/stream flags
+    fin.clear();
+    stream.flush();
+
+    // Read file
+    if ( fin.good() )
+    {
+        stream << fin.rdbuf();  // read buffer into stream
+        src = stream.str();     // load stream into source string
+    }
+    else
+    {
+        // Reading file failed somehow, print error message and quit
+        printf( "File %s could not be read...\n", path.c_str() );
+        return 0;
+    }
+
+    // TODO do stuff with model
+
+    return vertices;
+}
+
+void Model::Orbit( glm::mat4 origin, float dt, bool ccw, float speed )
+{
+    if (ccw)
+        angleTranslation += dt * M_PI/2.0; // Move through 90 degrees a second
+    else
+        angleTranslation -= dt * M_PI/2.0; // Move through 90 degrees a second
+
+	// Update the model matrix
+	model = glm::translate( origin,
+						    glm::vec3( lat_radius * sin(angleTranslation * speed),
+									   0.0f,
+									   long_radius * cos(angleTranslation * speed)
+								     )
+						   );
+}
+
+void Model::Spin( float dt )
+{
+    // Check spinning flag
+    if (isSpinning)
+    {
+        // Check which direction it's spinning
+        if (spinDirection)
+            angleRotation -= dt * M_PI/2;
+        else
+            angleRotation += dt * M_PI/2;
+    }
+
+    // Update the model matrix
+    model = glm::rotate(model, 2*angleRotation, glm::vec3(0.0f, 1.0f, 0.0f));
+}
+
+void Model::SwitchSpinDirection()
+{
+	spinDirection = !spinDirection;
+}
+
+void Model::SwitchSpinDirection( bool ccw )
+{
+	spinDirection = ccw;
+}
+
+bool Model::ToggleSpin()
+{
+	isSpinning = !isSpinning;
+	return isSpinning;
+}
+
+void Model::Scale( float scale )
+{
+    model = glm::scale( model, glm::vec3(scale,scale,scale) );
+}
+
+/** Getters & Setters **/
+void Model::SetOrbit( float latitude, float longitude )
+{
+	lat_radius = latitude;
+	long_radius = longitude;
+}
+
+void Model::SetSpinning( bool spin )
+{
+	isSpinning = spin;
+}
+
